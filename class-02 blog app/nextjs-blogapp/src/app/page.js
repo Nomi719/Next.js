5801bc60f8206388426f1effb6a30cd9c1ac606e
noman719 <mnomanmirz1996@gmail.com>
2024-10-01 00:56:17 +0500

    blog-app/using nextjs

---
+++ class-02 blog app/nextjs-blogapp/src/app/page.js
@@ -0,0 +1,18 @@
+// by default next js ke saare pages server side hote hen
+// server ko client side mein convert krne ke lye top pe "use client" lagaden
+// server side page ka console terminal pe show hoga
+// server side pe koi bhe react hook use nahn kr sakte.
+
+import PostCard from "./components/PostCard";
+
+export default async function Home() {
+  let posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+  posts = await posts.json();
+
+  // console.log("posts=>", posts);
+  return (
+    <div className="m-2">
+      {posts.map((data) =><PostCard data={data} />)}
+    </div>
+  );
+}
